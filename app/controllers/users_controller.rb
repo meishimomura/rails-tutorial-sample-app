fc7d3a5626a3eb4268182bc1181ca87ef574fdf9
meishimomura <mei.shimomura@icloud.com>
2021-06-02 11:50:35 +0900

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       # Success
       # GET "/users/#{@user.id}"
       flash[:success] = "Welcome to the Sample App!"
