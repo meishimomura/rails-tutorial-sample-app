1f05f09b4b7ad0dabb6da563e2fa133d3ad9089c
meishimomura <mei.shimomura@icloud.com>
2021-06-11 11:34:37 +0900

    Finish ch12

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -15,9 +15,9 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  # UserMailer.password_reset(self).deliver_now
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
 end
